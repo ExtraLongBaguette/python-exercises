3bfb2d87ac7f5a4a633668a552b2d6db5b82db0d
Lauri Lyytikäinen <lauri.lyytikainen1@gmail.com>
2021-10-24 19:22:09 +0300

    added pop

--- 3. Reading files/main.py
+++ 3. Reading files/main.py
@@ -6,6 +6,7 @@ def main():
     absPath = os.path.join(path, filePath)
     file = open(absPath)
     rows = file.readlines()
+    rows.pop()
     for i in range(len(rows)):
         data = rows[i].split(';')
         print('Kello oli', data[2].replace('\n', '') + ', kun punnittiin marjoja', data[0] + '.')
